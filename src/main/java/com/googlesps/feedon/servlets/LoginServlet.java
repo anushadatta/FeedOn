23c3bffd7c11cd4cdeaab526f919877ca4389b3f
Yew Onn <khaw.yewonn@gmail.com>
2020-07-15 14:29:41 +0800

    Restructure servlet's file structure

--- src/main/java/com/googlesps/feedon/servlets/LoginServlet.java
+++ src/main/java/com/googlesps/feedon/servlets/LoginServlet.java
@@ -1,4 +1,4 @@
-package com.google.sps.servlets;
+package com.googlesps.feedon.servlets;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
